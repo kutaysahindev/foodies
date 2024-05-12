5a2e300dd6509aaf510e8eda4f9a23d0dcbe895b
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-05-12 16:34:10 +0300

    Create header component & routes

---
+++ app/community/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function CommunityPage() {
+  return (
+    <>
+      <h1>Community Page</h1>
+      <Link href="/meals"> go meals</Link>
+    </>
+  );
+}
